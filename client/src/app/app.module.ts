cb50595e8704bb1affd6bebd34f7f4d1d902681c
Nursakinah <nursakinah.arz@gmail.com>
2023-02-24 10:48:53 +0800

    modified approutingmodule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
 import { CuisineListComponent } from './components/cuisine-list.component';
 import { RestaurantCuisineComponent } from './components/restaurant-cuisine.component';
 import { RestaurantDetailsComponent } from './components/restaurant-details.component';
+import { AppRoutingModule } from './components/app.routing.module';
 
 @NgModule({
   declarations: [
